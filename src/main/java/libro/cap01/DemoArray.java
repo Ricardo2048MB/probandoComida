999243401b9049556c19e68418174992ab7c3413
HP <HP@Ricardo04>
2023-09-25 16:41:59 -0600

    Segunda versión de los arrays. En esta versión se instanció una lista de días como una lista de cadenas de caracteres autodimensionada.

--- src/main/java/libro/cap01/DemoArray.java
+++ src/main/java/libro/cap01/DemoArray.java
@@ -4,20 +4,14 @@ import java.util.Scanner;
 
 public class DemoArray{
     public static void main(String[] args){
-        int arr[] = new int [10];
+        String dias[] = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"};
         Scanner lector = new Scanner(System.in);
-        System.out.println("Ingrese un valor (número cero para terminar): ");
+        System.out.println("Ingrese el día de la semana (como número): ");
         int v = lector.nextInt();
-        int i = 0;
-        //mientras v sea distinto de cero y i sea menor que diez
-        while(v != 0 && i <= 10){
-            arr[i++] = v;
-            System.out.println("Ingrese el siguiente valor (número cero para terminar): ");
-            v = lector.nextInt();            
-        }
-        //se recorre el arreglo mostrando todo su contenido
-        for(int j = 0; j <= i; j++){
-            System.out.println(arr[j]);
+        if(v <= dias.length){
+            System.out.println(dias[v-1]);
+        }else{
+            System.out.println("Día incorrecto…");
         }
     }
 }
